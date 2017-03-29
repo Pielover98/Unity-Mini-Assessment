7e977420a0cf2b269664314e6cf5ad59e2c43eb2
Xavier Melton <xavier.melton@ad.aie.edu>
2017-03-29 10:16:06 -0700

    Finished with Final Release

--- Assets/Scripts/DestroyRespawn.cs
+++ Assets/Scripts/DestroyRespawn.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 public class DestroyLavaBall : MonoBehaviour
 {
     public GameObject LavaBall;
+    private AudioSource audioSource;
+    public AudioClip BallSizzle;
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Destroy")
         {
+            audioSource = GetComponent<AudioSource>();
+            audioSource.clip = BallSizzle;
+            audioSource.Play();
             Destroy(LavaBall);
         }
     }
---
+++ Assets/Scripts/UpDownPlatformMove.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpDownPlatformMove : MonoBehaviour
+{
+    public float speed = 3;
+
+    float dir = 1;
+
+    void Update()
+    {
+
+        transform.Translate(new Vector3(0, 1, 0) * Time.deltaTime * dir * speed);
+    }
+
+
+    void OnTriggerEnter(Collider coll)
+    {
+        if (coll.gameObject.tag == "Reverse")
+        {
+            dir *= -1;
+        }
+    }
+
+}
